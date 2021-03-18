2254622679c2783312fced9d07d5d00ceeb3fa32
shivaneegupta <sgupta@gammastack.com>
2021-03-18 16:54:32 +0530

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "homes#welcome"
+  root to: "homes#welcome"
+  #root to: 'users#new'
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
 end
