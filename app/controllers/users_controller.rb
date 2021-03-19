94215d0244643f005db3447d4297f20969e62da6
shivaneegupta <sgupta@gammastack.com>
2021-03-19 15:10:00 +0530

    fixed indentatiosn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
-        render :new
+      render :new
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: "homes#welcome"
-  #root to: 'users#new'
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
 end
