ae7da69e9ec96d41d2221ede769faa50957aca80
Gupta-Shivanee <79202689+Gupta-Shivanee@users.noreply.github.com>
2021-03-20 09:14:35 +0530

    Bootstrap Integration (#2)
    * first commit of authentication
    
    * second commit of authentication
    
    * Use resource routing for session
    
    * separate controller added for welcome action
    
    * Added Bootstrap integration
    
    * custom styles in a separate file
    
    * routes change
    
    * fixed indentatiosn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+  validates :full_name, presence: true 
   validates :mobile_no, presence: true, numericality: true, length: { is: 10 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "homes#welcome"
+  root to: "homes#welcome"
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
 end
