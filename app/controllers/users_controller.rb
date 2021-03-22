3e3b4e9ec722d27cb3d7bae0191203d40d0cc68d
shivaneegupta <sgupta@gammastack.com>
2021-03-22 15:36:05 +0530

    added profile management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,21 +30,6 @@ class UsersController < ApplicationController
     end
   end
   
-  def edit_password
-    @user = User.find_by_id(session[:user_id])
-  end
-  
-  def update_password
-    @user = User.find_by_id(session[:user_id])
-    if @user&.authenticate(password_params[:current_password]) && @user.update(password: password_params[:password])
-      flash[:notice] = "password updated"
-      redirect_to user_path(@user.id)
-    else
-      flash[:notice] = "password updation failed!"
-      render :edit_password
-    end
-  end
-  
   private
   def signup_params
     params.require(:user).permit(:full_name, :email, :password, :gender, :mobile_no)
@@ -52,9 +37,5 @@ class UsersController < ApplicationController
   
   def user_update_params
     params.require(:user).permit(:full_name, :password, :mobile_no)
-  end
-  
-  def password_params
-    params.require(:user).permit(:password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   root to: "homes#welcome"
   resources :users, only: %i[new create edit update edit_password update_password show]
   resources :sessions, only: %i[new create destroy]
-  resources :users do
-    get :edit_password, on: :member
-  end
-
 end
