349e4a7004951648dcd933a4aca9170c4981d828
shivaneegupta <sgupta@gammastack.com>
2021-03-22 11:29:00 +0530

    added Profile Management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,46 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find_by_id(session[:user_id])
+  end
+  
+  def update
+    @user = User.find_by_id(session[:user_id])
+    if @user.update(user_update_params)
+      flash[:notice] = "profile updated"
+      redirect_to user_path(@user.id)
+    else
+      flash[:notice] = "profile updation failed"
+      render :edit
+    end
+  end
+  
+  def edit_password
+    @user = User.find_by_id(session[:user_id])
+  end
+  
+  def update_password
+    @user = User.find_by_id(session[:user_id])
+    if @user&.authenticate(password_params[:current_password]) && @user.update(password: password_params[:password])
+      flash[:notice] = "password updated"
+      redirect_to user_path(@user.id)
+    else
+      flash[:notice] = "password updation failed!"
+      render :edit_password
+    end
+  end
+  
   private
   def signup_params
     params.require(:user).permit(:full_name, :email, :password, :gender, :mobile_no)
+  end
+  
+  def user_update_params
+    params.require(:user).permit(:full_name, :password, :mobile_no)
+  end
+  
+  def password_params
+    params.require(:user).permit(:password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "homes#welcome"
-  #root to: 'users#new'
-  resources :users, only: %i[new create show]
+  resources :users, only: %i[new create edit update edit_password update_password show]
   resources :sessions, only: %i[new create destroy]
+  resources :users do
+    get :edit_password, on: :member
+  end
+
 end
