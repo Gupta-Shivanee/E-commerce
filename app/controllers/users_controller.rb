40c52eec99f21cd7a9ffc600a7cff54c822eb365
shivaneegupta <sgupta@gammastack.com>
2021-03-24 21:29:24 +0530

    fixed indentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,16 +11,16 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
-        render :new
+      render :new
     end
   end
   
   def edit
-    @user = User.find_by_id(session[:user_id])
+    @user = current_user
   end
   
   def update
-    @user = User.find_by_id(session[:user_id])
+    @user = current_user
     if @user.update(user_update_params)
       flash[:notice] = "profile updated"
       redirect_to user_path(@user.id)
@@ -35,6 +35,6 @@ class UsersController < ApplicationController
   end
   
   def user_update_params
-    params.require(:user).permit(:full_name,:password, :image, :mobile_no)
+    params.require(:user).permit(:full_name, :password, :image, :mobile_no)
   end
 end
