c4c3d01db78bae60dc7dcd29fb8f9322a1685949
shivaneegupta <sgupta@gammastack.com>
2021-03-31 18:17:12 +0530

    removed user_id validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
   has_many_attached :image, dependent: :destroy
   belongs_to :user
   
-  validates :name, :user_id, :quantity, :description, presence: true
+  validates :name, :quantity, :description, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
 end
