508e8706d321a91d11be146e06ccded7f66479ac
Gupta-Shivanee <79202689+Gupta-Shivanee@users.noreply.github.com>
2021-03-31 19:27:43 +0530

    Addproduct (#6)
    * first commit of authentication
    
    * second commit of authentication
    
    * Use resource routing for session
    
    * separate controller added for welcome action
    
    * Added Bootstrap integration
    
    * custom styles in a separate file
    
    * routes change
    
    * added Profile Management
    
    * added profile management
    
    * added profile management
    
    * added profile management
    
    * added active storage
    
    * added products
    
    * fixed indentation
    
    * removed user_id validation

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ApplicationRecord
+  has_many_attached :image, dependent: :destroy
+  belongs_to :user
+  
+  validates :name, :quantity, :description, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "homes#welcome"
   resources :users, only: %i[new create edit update show]
   resources :sessions, only: %i[new create destroy]
+  resources :products, only: %i[index new create show]
 end
